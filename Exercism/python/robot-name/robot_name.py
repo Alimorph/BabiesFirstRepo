a4b5f9a9ce51263fe12f399fca43bf575565e9eb
alimorph <alymorgan@hotmail.co.uk>
2019-01-03 03:10:01 +0000

    Completed robot name

--- Exercism/python/robot-name/robot_name.py
+++ Exercism/python/robot-name/robot_name.py
@@ -1,3 +1,21 @@
+import random
+
 class Robot(object):
     def __init__(self):
-        pass
+        self.name = self.genName()
+    
+    def genName(self):
+        number = random.randint(0,1000)
+        if number < 10:
+            strNumber = '00'+str(number)
+        elif number < 100:
+            strNumber = '0'+str(number)
+        else:
+            strNumber = str(number)
+        letter1 = chr(random.randint(65,91))
+        letter2 = chr(random.randint(65,91))
+        return letter1 + letter2 + strNumber
+
+    def reset(self):
+        random.seed('Alimorph The Almighty')
+        self.name = self.genName()
